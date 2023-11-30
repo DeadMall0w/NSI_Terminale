56f0de8c386e64e1f2231289cfdb249cd6544e5d
Raphael Berquier <100719400+DeadMall0w@users.noreply.github.com>
2023-11-30 11:37:51 +0100

    Add files via upload

--- Programmation_dynamique.py
+++ Programmation_dynamique.py
@@ -17,3 +17,49 @@ def fib_mem_c(n,m):
   else:
     m[n]=fib_mem_c(n-1,m) + fib_mem_c(n-2,m)
     return m[n]
+  
+def algo_glouton(m , S=[500,200,100,50,20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01]):
+  i = 0
+  T=[]
+  while m != 0 :
+    if m >= S[i] :
+      T.append(S[i])
+      m = round(m - S[i],2)
+    else :
+      i += 1
+  return T
+
+def rendu_monnaie_rec(P,X):
+  if X==0:
+    return 0
+  else:
+    mini = 1000
+  for i in range(len(P)):
+    if P[i]<=X:
+      nb = 1 + rendu_monnaie_rec(P,X-P[i])
+      if nb<mini:
+        mini = nb
+  return mini
+
+pieces = (2,5,10,50,100)
+
+def rendu_monnaie_mem(P,X):
+  mem = [0]*(X+1)
+  return rendu_monnaie_mem_c(P,X,mem)
+
+def rendu_monnaie_mem_c(P,X,m):
+  if X==0:
+    return 0
+  elif m[X]>0:
+    return m[X]
+  else:
+    mini = 1000
+    for i in range(len(P)):
+      if P[i]<=X:
+        nb=1+rendu_monnaie_mem_c(P,X-P[i],m)
+        if nb<mini:
+          mini = nb
+          m[X] = mini
+  return mini
+
+pieces = (2,5,10,50,100) 
